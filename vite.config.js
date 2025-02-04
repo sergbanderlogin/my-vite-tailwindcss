716f69fce31773a92c20c015cba12dd036778094
SergeyBunakov <bunakov.sergey@gmail.com>
2025-02-04 15:42:35 +0200

    added plugin @tailwindcss/vite in vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,46 @@
-import Inspector from "vite-plugin-inspect"
-import { defineConfig } from 'vite'
-import { sync } from 'glob'
+import Inspector from "vite-plugin-inspect";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { sync } from "glob";
 
 const noAttr = () => {
-    return {
-        transformIndexHtml(html) {
-            return html.replaceAll(' crossorigin', '')
-        }
+  return {
+    transformIndexHtml(html) {
+      return html.replaceAll(" crossorigin", "");
     }
-}
+  };
+};
 
 export default defineConfig({
-    plugins: [
-        noAttr(),
-        Inspector(),
-    ],
-    server: {
-        open: "/index.html",
-    },
-    build: {
-        rollupOptions: {
-            input: sync('src/**/*.html'.replace(/\\/g, '/')),
-            output: {
-                assetFileNames: (assetInfo) => {
-                    let extType = assetInfo.name
-                    if (/css/.test(extType)) { extType = 'assets/css' }
-                    return assetInfo.originalFileName ?? `${extType}/[name][extname]`
-                },
-                chunkFileNames: 'assets/js/[name].js',
-                entryFileNames: 'assets/js/[name].js',
-            },
+  plugins: [
+    tailwindcss(),
+    noAttr(),
+    Inspector()
+  ],
+  server: {
+    open: "/index.html"
+  },
+  build: {
+    rollupOptions: {
+      input: sync("src/**/*.html".replace(/\\/g, "/")),
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name;
+          if (/css/.test(extType)) {
+            extType = "assets/css";
+          }
+          return assetInfo.originalFileName ?? `${extType}/[name][extname]`;
         },
-        assetsInlineLimit: 0,
-        emptyOutDir: true,
-        outDir: '../dist',
+        chunkFileNames: "assets/js/[name].js",
+        entryFileNames: "assets/js/[name].js"
+      }
     },
-    root: 'src',
-    base: '',
-})
+    assetsInlineLimit: 0,
+    emptyOutDir: true,
+    outDir: "../dist"
+  },
+  root: "src",
+  base: ""
+});
 
 
